220f978bef833bf8f68d14ac8518dc06c23ca751
samith <samithks@hotmail.com>
2020-06-14 18:54:35 +0530

    Added configuration validation

--- src/config/configuration.module.ts
+++ src/config/configuration.module.ts
@@ -3,11 +3,18 @@ import { Module } from '@nestjs/common';
 import configuration from './configuration';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_PATH } from '../config/constant';
+import * as Joi from '@hapi/joi';
 
 @Module({
     imports: [ConfigModule.forRoot({
         envFilePath: ENV_PATH,
         load: [configuration],
+        validationSchema: Joi.object({
+            NODE_ENV: Joi.string()
+                .valid('development', 'production', 'test', 'provision')
+                .default('development'),
+            PORT: Joi.number().default(3000),
+        }),
     })]
 })
 export class ConfigurationModule { }
