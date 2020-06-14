83721e5a47b06a13ed4209a33097611001941665
samith <samithks@hotmail.com>
2020-06-14 19:10:54 +0530

    Get the port with config service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT'));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
