d0b2d653121e95ec522d0b0908a350e07bacfa69
samith <samithks@hotmail.com>
2020-06-14 17:59:27 +0530

    Created a module for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseConnectionModule } from './connections/database/database-connection.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseConnectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
