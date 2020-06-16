fc3d2199bef2b327528b1c474914f184b0797b64
samith <samithks@hotmail.com>
2020-06-16 22:44:26 +0530

    added morgan and access logging

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { createStream } from 'rotating-file-stream';
+import path from 'path';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +18,16 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
+
+  // create a rotating write stream
+  const accessLogStream = createStream('access.log', {
+    size: '10M', // rotate every 10 MegaBytes written
+    interval: '1d', // rotate daily,
+    compress: 'gzip', // compress rotated files
+    maxFiles: 10,
+    path: path.join(__dirname, '../../logs')
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
   SwaggerModule.setup('api', app, document);
   await app.listen(configService.get<number>('PORT'));
   console.log(`Application is running on: ${await app.getUrl()}`);
